64354cfbcd859de4178ef7b29805eb75639f6b55
msalamina <msalamina@yahoo.com>
2016-11-13 08:34:07 -0500

    Merge teh training and test data sets and returns a tidy data set

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+
+## R script called run_analysis.R that does the following.
+
+# setwd("C:/Users/mike/R/CleaningData")
+# Load the data
+
+features <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
+trainData <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+testData <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+trainDataY <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
+testDataY <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
+
+activityLabels <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
+
+
+subjectTestData <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
+subjectTrainData <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
+
+
+#features <- cbind(features, c["562", "activity"])
+names(trainData) <- features[,2]
+names(testData) <- features[,2]
+
+mergeTrainData <- cbind(subjectTrainData, trainDataY, trainData)
+mergeTestData <- cbind(subjectTestData, testDataY, testData)
+#mergeTestData <- cbind(testData, testDataY)
+
+#  Merges the training and the test sets to create one data set.
+mergeData <- rbind(mergeTrainData, mergeTestData)
+
+#  Uses descriptive activity names to name the activities in the data set
+names(mergeData)[2] <- "ActivityCode"
+names(mergeData)[1] <- "SubjectNumber"
+
+labelledMergeData <- merge(activityLabels, mergeData, by.y = "ActivityCode", by.x = "V1")
+names(labelledMergeData)[2] <- "ActivityName"
+
+#  Extracts only the measurements on the mean and standard deviation for each measurement.
+tempResult <- labelledMergeData[grep("mean|std|Activity|Subject", names(labelledMergeData))]
+
+#  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Write data set
+write.table(tempResult, file="MovementStatistics.csv", row.name=FALSE, sep=",")
+
